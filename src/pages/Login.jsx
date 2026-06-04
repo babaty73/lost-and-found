ebbddc527f0e2d961bc62b46bae2a6f49f490597
imran <imranidris10999@gmail.com>
2026-06-04 04:53:39 -0700

    improved responsiveness in the item cards image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Auth.css";
+import "./Login.css";
 
 function Login({ setIsLoggedIn }) {
   const navigate = useNavigate();
