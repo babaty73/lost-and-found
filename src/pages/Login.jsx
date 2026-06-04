79d55b407f2bd628a73ca707bb004368f89503dc
imran <imranidris10999@gmail.com>
2026-06-04 05:02:14 -0700

    improved the login page UI and debugged the white border issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
+import "./Auth.css";
 
 function Login({ setIsLoggedIn }) {
   const navigate = useNavigate();
