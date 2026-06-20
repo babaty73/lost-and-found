8e29d0500b3510b06bb67bb24494ef79d2d48264
imran <imranidris10999@gmail.com>
2026-06-20 04:41:11 -0700

    added server for backend development

---
+++ backend/controllers/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
