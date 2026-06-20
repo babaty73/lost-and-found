49ee475ddc17431bfa0101b480b04d6539f4d877
imran <imranidris10999@gmail.com>
2026-06-20 04:51:13 -0700

    protected routes with JWT

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
+import itemRoutes from "./routes/items.js";
+
 
 dotenv.config();
 
@@ -9,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
