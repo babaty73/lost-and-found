d488cea0b7b707d2792174d846145760b42d127e
imran <imranidris10999@gmail.com>
2026-06-20 09:06:36 -0700

    fixed the limit issue of reported items

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
 
